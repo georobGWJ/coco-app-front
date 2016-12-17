26889fa2d01773891c2cfeff199e84fc80554fb5
Robert Turner Jr <georob23@gmail.com>
2016-12-17 14:21:58 -0800

    Initial commit of skeleton front-end with user authentication.

---
+++ tests/unit/helpers/session-object-formatter-test.js
@@ -0,0 +1,10 @@
+import { sessionObjectFormatter } from 'coco-frontend/helpers/session-object-formatter';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | session object formatter');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = sessionObjectFormatter([42]);
+  assert.ok(result);
+});
