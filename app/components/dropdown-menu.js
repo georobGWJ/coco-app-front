93c84d4c19047addb9a2ac604b8b44daeee71056
Robert Turner Jr <georob23@gmail.com>
2016-12-17 18:06:00 -0800

    Delete unneeded file.

--- app/components/dropdown-menu.js
+++
@@ -1,5 +0,0 @@
-{{#select-box value=attrs.value on-select=attrs.on-select class-prefix='my-select-box' as |sb|}}}
-  {{sb.selected-option label=sb.value.name}}
-  <button onclick={{action sb.toggle}}>Toggle</button>
-  {{yield sb}}
-{{/select-box}}
